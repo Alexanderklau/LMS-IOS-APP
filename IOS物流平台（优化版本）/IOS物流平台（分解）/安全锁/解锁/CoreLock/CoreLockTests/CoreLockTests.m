87d674de6b310e14b4022f1aebc9cada802cc757
Klichko Yemilice <429095816@qq.com>
2016-12-21 12:19:33 +0800

    Update CoreLockTests.m

--- IOS物流平台（优化版本）/IOS物流平台（分解）/安全锁/解锁/CoreLock/CoreLockTests/CoreLockTests.m
+++ IOS物流平台（优化版本）/IOS物流平台（分解）/安全锁/解锁/CoreLock/CoreLockTests/CoreLockTests.m
@@ -1,10 +1,4 @@
-//
-//  CoreLockTests.m
-//  CoreLockTests
-//
-//  Created by 成林 on 15/4/21.
-//  Copyright (c) 2015年 冯成林. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
